243556cf2fa3879fdd329c4d4894dd37bdcc7dd7
janhaviDadhania <janhavivdadhania@gmail.com>
2021-01-19 22:25:04 +0530

    best first search bfs main loop added

--- Assignment2/6_original.py
+++ Assignment2/6_original.py
@@ -18,13 +18,72 @@ class candidate_design:
         self.from_ = from_
         self.to_ = to_
 
+    def Best_first_search(self, goal_design, start_design):
+
+        goal = deepcopy(goal_design)
+        start = deepcopy(start_design)
+        queue = []
+        visited = []
+        queue.append(start)
+
+        while len(queue):
+            current = queue.pop()
+            visited.append(unique_identifier(current.design))
+
+            if goalTest(current.design, goal.design) == True:
+                #traching back to reach initial state
+                while current != None:
+                    print(current.design)
+                    current = current.parent
+                return
+
+            children = movGen(current)
+            sort_(children)
+            for child in children:
+                id = unique_identifier(child.design)
+                if not (id in visited):
+                    child.parent = current
+                    queue.append(child)
+
+def unique_identifier(design):
+    """
+    input: state representation
+    output: unique identifier string for state
+    """
+
+    string = ""
+    for stack in design:
+        for element in stack:
+            string = ''.join([string,str(element)])
+        string = ''.join([string,"|"])
+    return string
+
+def sort_(children):
+    """
+    sort the children according to selected heuristic
+    """
+
+    return children
 
 def goalTest(design_one, GoalDesign):
+    """
+    Function to test if the current state is goal state
+    return True if it is False otherwise
+    """
+
     if(design_one != None and GoalDesign != None):
-        return design_one == GoalDesign
+        if(unique_identifier(design_one) == unique_identifier(GoalDesign)):
+            return True
+        if(design_one == GoalDesign) :
+            return True
     return False
 
 def movGen(design):
+    """
+    constraints: maximum allowed states = 3
+    Input: takes state representation as input
+    Output: returns list of all neighbouts (possible transitions) from current state
+    """
 
     children = []
 
@@ -46,6 +105,7 @@ def heuristic(heuristic_number, design, goalDesign):
     heuristic_number 1 : underestimating
     heuristic_number 2 : overestimating
     """
+    
     temp = deepcopy(design)
     goal = deepcopy(goalDesign)
     cost = 0
@@ -58,9 +118,15 @@ def heuristic(heuristic_number, design, goalDesign):
             cost += abs(len(stack) - len(goal_stack))
             print("cost : ",cost)
     return cost
-
+#
 # yo = candidate_design([[1], [2], [3]], None, 0, 0)
-# yoyo = candidate_design([[1,2], [], [3]], None, 0, 0)
+# yoyo = candidate_design([[3,2], [], [1]], None, 0, 0)
+# yo.Best_first_search(yoyo, yo)
+
+# state = yoyo
+# while state != None:
+#     print(state.design)
+#     state = state.parent
 # print(heuristic(1,yo,yoyo))
 
 # children = movGen(yo)
